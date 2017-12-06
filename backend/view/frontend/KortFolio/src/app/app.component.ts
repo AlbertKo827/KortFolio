23b50e431a880bbcae1582dbdbaf5363e7033545
Myeong Seok Ko <myeongsku@gmail.com>
2017-12-06 19:47:15 +0900

    Create TitleBar Compnent

--- backend/view/frontend/KortFolio/src/app/app.component.ts
+++ backend/view/frontend/KortFolio/src/app/app.component.ts
@@ -5,10 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [
     './app.component.css',
-    '../../node_modules/bootstrap/dist/css/bootstrap.css',
-    '../../node_modules/bootstrap-social/bootstrap-social.css',
-    '../../node_modules/bootstrap-social/assets/css/docs.css',
-    '../../node_modules/bootstrap-social/assets/css/font-awesome.css'
   ]
 })
 export class AppComponent {
