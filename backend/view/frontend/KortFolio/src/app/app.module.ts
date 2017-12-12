3a88d12fbf5cb9b0c90311fb1388062b10f5ea1e
Myeong Seok Ko <myeongsku@gmail.com>
2017-12-13 01:37:41 +0900

    DropDown Success

--- backend/view/frontend/KortFolio/src/app/app.module.ts
+++ backend/view/frontend/KortFolio/src/app/app.module.ts
@@ -6,15 +6,19 @@ import { LoginComponent } from './login/login.component';
 import { TitlebarComponent } from './titlebar/titlebar.component';
 import { CtrlSBBComponent } from './ctrl-sbb/ctrl-sbb.component';
 
+import { BsDropdownModule as DropDown} from 'ngx-bootstrap';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     TitlebarComponent,
-    CtrlSBBComponent
+    CtrlSBBComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropDown.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
