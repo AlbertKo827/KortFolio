0586d10b3687b8f70faf526aa503e2afb4bab6e9
Myeong Seok Ko <myeongsku@gmail.com>
2018-02-07 14:05:43 +0900

    코드 구성 변경

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,16 @@ const cookieparser = require('cookie-parser');
 const Mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 const Config = require('./module/configuration.js');//Configration Data e.g>host url, key value...
+
 const route = require('./routes')//RESTful API
+
+//PassPort
+const passport = require('passport');
+
+const kakaoStrategy = require('passport-kakao').Strategy;
+const naverStrategy = require('passport-naver').Strategy;
+const facebookStrategy = require('passport-facebook').Strategy;
+/////////////////////////////////////
 
 app.locals.pretty = true;
 
@@ -19,9 +28,45 @@ app.set('views', './view/frontend/KortFolio/dist');
 app.engine('html', require('ejs').renderFile);
 Config.KFInitialize(app, Mongoose, session, MongoStore);
 
+//#region PassPort
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id ,done)=>{
+    User.findById(id, (err, user)=>{
+        done(err, user);
+    })
+});
+
+passport.use('naver', new naverStrategy(Config.naverValue, 
+    (accessToken, refreshToken, profile, done)=>
+    {
+        done(null, profile);
+    }
+));
+
+passport.use('facebook', new facebookStrategy(Config.facebookValue, 
+    (accessToken, refreshToken, profile, cb)=>
+    {
+
+    }
+));
+
+passport.use('kakao', new kakaoStrategy(Config.kakaoValue, 
+    (accessToken, refreshToken, profile, done)=>
+    {
+        console.log(profile);
+    }
+));
+//#endregion
+//////////////////////////////////////////////
 
 
-route(app);
+route(express, passport);
 // require('./routes/login.js').loginRoute(app);
 
 Config.serverOn(app);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,75 +1,13 @@
-const express = require('express');
-const passport = require('passport');
-
-const kakaoStrategy = require('passport-kakao').Strategy;
-const naverStrategy = require('passport-naver').Strategy;
-const facebookStrategy = require('passport-facebook').Strategy;
-
-const login = require('./login.js').loginRoute(express.Router(), passport);
 const restAPI = require('./RESTfulAPI');
-const config = require('../module/configuration.js');
 
-module.exports = (app=>{//Test
-app.use(passport.initialize());
+module.exports = ((express, passport)=>{//Test
+    const app = express();
 
-    app.get('/', (req, res)=>{
-        res.render('index');
-    })
+    const login = require('./login.js').loginRoute(express.Router(), passport);
 
-    app.get('/home',(req, res)=>{
-        res.send(`<h1>Fuck it...</h1>`);
-    })
+app.get('/',(req, res)=>{
+    res.render('index');
+})
 
-    app.post('/fail',(req, res)=>{
-        res.send('ok');
-    } )
-
-    app.post('/home',(req, res)=>{
-        res.send('ok');
-    } )
-
-/*
-    app.post('/',(req, res)=>{
-        res.redirect('/home');
-    })
-
-    app.get('/home',(req, res)=>{
-        res.send(`<h1>here is home</h1>`);
-    })
-*/
     app.use('/login', login);
-///////////////////////////////////////////
-    // app.use(passport.initialize());
-    app.use(passport.session());
-
-    passport.serializeUser((user, done)=>{
-        done(null, user.id);
-    });
-
-    passport.deserializeUser((id ,done)=>{
-        User.findById(id, (err, user)=>{
-            done(err, user);
-        })
-    });
-
-    passport.use('naver', new naverStrategy(config.naverValue, 
-        (accessToken, refreshToken, profile, done)=>
-        {
-            done(null, profile);
-        }
-    ));
-
-    passport.use('facebook', new facebookStrategy(config.facebookValue, 
-        (accessToken, refreshToken, profile, cb)=>
-        {
-    
-        }
-    ));
-
-    passport.use('kakao', new kakaoStrategy(config.kakaoValue, 
-        (accessToken, refreshToken, profile, done)=>
-        {
-            console.log(profile);
-        }
-    ));
 })
