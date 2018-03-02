20ab3af0c54b2c0dc18b2eccbd7e9853b2a43dd8
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-03 01:45:15 +0900

    passport kakao, facebook add & add logout

--- backend/app.js
+++ backend/app.js
@@ -98,16 +98,88 @@ passport.use('naver', new naverStrategy(Config.naverValue,
 ));
 
 passport.use('facebook', new facebookStrategy(Config.facebookValue, 
-    (accessToken, refreshToken, profile, cb)=>
+    (accessToken, refreshToken, profile, done)=>
     {
+        var _profile = profile._json;
 
+        console.log(profile);
+
+        User.findOne({//DB에서 회원 정보를 찾는다.
+            '_id': profile.id
+        }, (err, user)=>{
+
+            if(err){//DB Error!
+                console.log("에러! : " + err);
+
+                return done(err);
+            }
+            else if (!user) {//회원이 아니라면
+                const _user = new User({//신규 유저 정보를 JSON으로
+                    _id : _profile.id,
+                    _name: profile.displayName,
+                    _email: _profile.email
+                });
+
+                _user.save((err)=>{
+
+                    if (err){ 
+                        console.log(err);
+                        console.log('done 1');
+                        return done(err);
+                    }
+                    else{
+                        console.log('done 2');
+                        return done(null, user);
+                    }
+                });
+            } else {
+                console.log('로그인');
+                return done(null, user);
+            }
+        });
     }
 ));
 
 passport.use('kakao', new kakaoStrategy(Config.kakaoValue, 
     (accessToken, refreshToken, profile, done)=>
     {
-        
+        var _profile = profile._json;
+
+        console.log(profile);
+
+        User.findOne({//DB에서 회원 정보를 찾는다.
+            '_id': profile.id
+        }, (err, user)=>{
+
+            if(err){//DB Error!
+                console.log("에러! : " + err);
+
+                return done(err);
+            }
+            else if (!user) {//회원이 아니라면
+                const _user = new User({//신규 유저 정보를 JSON으로
+                    _id : _profile.id,
+                    _name: profile.displayName,
+                    _email: _profile.email
+                });
+
+                _user.save((err)=>{
+
+                    if (err){ 
+                        console.log(err);
+                        console.log('done 1');
+                        return done(err);
+                    }
+                    else{
+                        console.log('done 2');
+                        return done(null, user);
+                    }
+                });
+            } else {
+                console.log('로그인');
+                return done(null, user);
+            }
+        });
     }
 ));
 //#endregion
@@ -128,6 +200,34 @@ app.get('/login/naver_oauth', passport.authenticate('naver', {
     //next();
 })
 
+app.get('/login/auth/kakao', passport.authenticate('kakao', null),
+        (req, res)=>{
+            console.log('connect kakao');
+        })
+
+app.get('/login/kakao_oauth', passport.authenticate('kakao', {
+    failureRedirect : '/login'
+}), (req, res, next)=>{
+    console.log('logining');
+    //res.end();
+    res.redirect('/');
+    //next();
+})
+
+app.get('/login/auth/facebook', passport.authenticate('facebook', null),
+        (req, res)=>{
+            console.log('connect facebook');
+        })
+
+app.get('/login/facebook_oauth', passport.authenticate('facebook', {
+    failureRedirect : '/login'
+}), (req, res, next)=>{
+    console.log('logining');
+    //res.end();
+    res.redirect('/');
+    //next();
+})
+
 app.get('/*', (req, res, next)=>{
     res.sendFile(path.join(__dirname, 'view/index.html'));
     console.log('sned!!');
@@ -137,6 +237,10 @@ app.get('/*', (req, res, next)=>{
 app.get('/login', (req, res, next)=>{
     console.log('hello');
 });
+
+app.get('/logout', (req, res)=>{
+    req.logout();
+})
 
 //app.use('/login', login);
 app.use('/register', register);
