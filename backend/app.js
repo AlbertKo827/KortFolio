c4bc756191ccc1f105cda21bd552880acf8b0eb4
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-08 22:23:28 +0900

    edit

--- backend/app.js
+++ backend/app.js
@@ -238,7 +238,10 @@ app.get('/mailtest', (req, res)=>{
 app.get('/*', (req, res, next)=>{
     res.sendFile(path.join(__dirname, 'view/index.html'));
     res.json(req.user._id);
-    console.log(req.user._id);
+    if(req.user)
+        console.log(req.user._id);
+    else
+        console.log("can't find")
     next();
 })
 
