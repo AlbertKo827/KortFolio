9a6fceaed4179a886641beab65f3ac1e5772557d
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-15 17:35:51 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -254,19 +254,19 @@ app.get('/logout', (req, res)=>{
 app.use('/register', register);
 
 app.get('/test', (req, res)=>{
-    //console.log(req.user);
+    console.log(req.user);
 
     // if(req.user != undefined){
     //     User.findById(req.user._id, (err, user)=>{
     //         var user = req.user;
     //         if(!err)
-    //             //res.json(user);
+                // res.json(user);
     //             console.log('okokok');
     //         else
     //             console.log("can't find")
     //     })
     // }
-    //res.send(req.user._name);
+    // res.send(req.user._name);
 })
 
 
