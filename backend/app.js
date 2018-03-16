f24d840aa206eee3e8efb333890c4dee21818e23
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-16 14:24:33 +0900

    json test

--- backend/app.js
+++ backend/app.js
@@ -255,7 +255,7 @@ app.get('/logout', (req, res)=>{
 app.use('/register', register);
 
 app.get('/test', (req, res)=>{
-    res.json({message : "success", username : req.user._name});
+    res.json({message : "success", username : "req.user._name"});
 
 
     // console.log(req.user);
