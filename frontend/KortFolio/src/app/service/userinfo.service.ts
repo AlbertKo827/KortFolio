0c559c1a9465d811f51e45b9c3b6631c59aff16f
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-17 03:21:44 +0900

    test

--- frontend/KortFolio/src/app/service/userinfo.service.ts
+++ frontend/KortFolio/src/app/service/userinfo.service.ts
@@ -16,7 +16,7 @@ export class UserinfoService {
   constructor(private http : Http) { }
 
   getUserName() : void {
-    this.http.get('./user')
+    this.http.get('/test')
              .map((res) => res.json())
              .subscribe(data => {
                console.log(data);
