6c368abc080a20708b8e885e5909241f7ba7eed3
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-19 00:04:31 +0900

    test

--- backend/view/main.bundle.js
+++ backend/view/main.bundle.js
@@ -515,7 +515,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/test/test.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<p>\n  Hello! : {{test}}\n</p>\n"
+module.exports = "<p>\n  Hello! : {{test?.message}}\n</p>\n"
 
 /***/ }),
 
