3d68adf2d6744748cecef348a6806c3c7cc0b577
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-22 00:56:57 +0900

    JSON error test

--- backend/app.js
+++ backend/app.js
@@ -249,7 +249,7 @@ app.get('/*', (req, res, next)=>{
 app.get('/test', (req, res, next)=>{
     //res.json({message : "success", username : "retgrtgrtgrtgrtgrtgbbbbb"});
     res.json(req.user);
-
+    console.log(req.user);
     // console.log(req.user);
 
     // if(req.user != undefined){
