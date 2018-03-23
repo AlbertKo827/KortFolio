655200e45ca1787244e24aba8d26ddc260cb8980
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-23 22:43:55 +0900

    edit error

--- backend/module/mailsend.js
+++ backend/module/mailsend.js
@@ -9,19 +9,19 @@ var transporter = nodemailer.createTransport(smtpTransport({
     }
 }));
 
-var mailOptions = (body) = {
-    from: 'myeongsku@gmail.com',
-    to: 'myeongsku@naver.com',
-    subject: 'test',
-    text: "이름 : " + body.name + "₩n" + "연락처 : " + body.tel + "₩n" + "내용 : " + body.body
-};
-
-module.exports.send = (option) => transporter.sendMail(mailOptions(option), (error, info)=>{
-    if (error){
-        console.log(error);
-    }
-    else {
-        console.log('Email sent! : ' + info.response);
-    }
-    transporter.close();
-});
+module.exports.send = (body) => {
+    transporter.sendMail({
+        from: 'myeongsku@gmail.com',
+        to: 'myeongsku@naver.com',
+        subject: 'test',
+        text: "이름 : " + body.name + "₩n" + "연락처 : " + body.tel + "₩n" + "내용 : " + body.body
+    }, (error, info)=>{
+        if (error){
+            console.log(error);
+        }
+        else {
+            console.log('Email sent! : ' + info.response);
+        }
+        transporter.close();
+    });
+}
