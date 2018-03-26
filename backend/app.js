fd4e6f39465f43c17aa296fa99dea4f2829685d3
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-26 16:46:05 +0900

    clean passport module

--- backend/app.js
+++ backend/app.js
@@ -10,18 +10,7 @@ const Config = require('./module/configuration.js');//Configration Data e.g>host
 
 const Mail = require('./module/mailsend.js');
 
-//PassPort
-//const passport = require('passport');
 const passport = require('./module/passport');
-
-
-const kakaoStrategy = require('passport-kakao').Strategy;
-const naverStrategy = require('passport-naver').Strategy;
-const facebookStrategy = require('passport-facebook').Strategy;
-/////////////////////////////////////
-
-//const route = require('./routes')//RESTful API
-//const index = require('./routes/index.js');
 
 const route = express.Router();
 
@@ -44,208 +33,14 @@ app.use(cookieparser(Config.secretCookie));
 
 Config.KFInitialize(app, Mongoose, session, MongoStore);
 
-
-
-
 // #region PassPort
 app.use(passport.initialize);
 app.use(passport.session);
 passport.setPassport();
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser((user, done)=>{
-//     console.log(user);
-//     done(null, user._id);
-// });
-
-// passport.deserializeUser((id ,done)=>{
-//     User.findById(id, (err, user)=>{
-//         done(err, user);
-//     })
-// });
-
-// passport.use('naver', new naverStrategy(Config.naverValue, 
-//     (accessToken, refreshToken, profile, done)=>
-//     {
-//         //console.log(profile);
-        
-//         var _profile = profile._json;
-
-//         User.findOne({//DB에서 회원 정보를 찾는다.
-//             '_id': profile.id
-//         }, (err, user)=>{
-
-//             if(err){//DB Error!
-//                 console.log("에러! : " + err);
-
-//                 return done(err);
-//             }
-//             else if (!user) {//회원이 아니라면
-//                 const _user = new User({//신규 유저 정보를 JSON으로
-//                     _id : _profile.id,
-//                     _name: profile.displayName,
-//                     _email: _profile.email,
-//                     _password : '1111'
-//                 });
-
-//                 _user.save((err)=>{
-
-//                     if (err){ 
-//                         console.log(err);
-//                         console.log('done 1');
-//                         return done(err);
-//                     }
-//                     else{
-//                         console.log('done 2');
-//                         return done(null, user);
-//                     }
-//                 });
-//             } else {
-//                 console.log('로그인');
-//                 return done(null, user);
-//             }
-//         });
-//     }
-// ));
-
-// passport.use('facebook', new facebookStrategy(Config.facebookValue, 
-//     (accessToken, refreshToken, profile, done)=>
-//     {
-//         var _profile = profile._json;
-
-//         console.log(profile);
-
-//         User.findOne({//DB에서 회원 정보를 찾는다.
-//             '_id': profile.id
-//         }, (err, user)=>{
-
-//             if(err){//DB Error!
-//                 console.log("에러! : " + err);
-
-//                 return done(err);
-//             }
-//             else if (!user) {//회원이 아니라면
-//                 const _user = new User({//신규 유저 정보를 JSON으로
-//                     _id : _profile.id,
-//                     _name: profile.displayName,
-//                     _email: _profile.email,
-//                     _password : '1111'
-//                 });
-
-//                 _user.save((err)=>{
-
-//                     if (err){ 
-//                         console.log(err);
-//                         console.log('done 1');
-//                         return done(err);
-//                     }
-//                     else{
-//                         console.log('done 2');
-//                         return done(null, user);
-//                     }
-//                 });
-//             } else {
-//                 console.log('로그인');
-//                 return done(null, user);
-//             }
-//         });
-//     }
-// ));
-
-// passport.use('kakao', new kakaoStrategy(Config.kakaoValue, 
-//     (accessToken, refreshToken, profile, done)=>
-//     {
-//         var _profile = profile._json;
-
-//         console.log(profile);
-
-//         User.findOne({//DB에서 회원 정보를 찾는다.
-//             '_id': profile.id
-//         }, (err, user)=>{
-
-//             if(err){//DB Error!
-//                 console.log("에러! : " + err);
-
-//                 return done(err);
-//             }
-//             else if (!user) {//회원이 아니라면
-//                 const _user = new User({//신규 유저 정보를 JSON으로
-//                     _id : _profile.id,
-//                     _name: profile.displayName,
-//                     _email: '',
-//                     _password : '1111'
-//                 });
-
-//                 _user.save((err)=>{
-
-//                     if (err){ 
-//                         console.log(err);
-//                         console.log('done 1');
-//                         return done(err);
-//                     }
-//                     else{
-//                         console.log('done 2');
-//                         return done(null, user);
-//                     }
-//                 });
-//             } else {
-//                 console.log('로그인');
-//                 return done(null, user);
-//             }
-//         });
-//     }
-// ));
-//#endregion
 
 
-
-// app.get('/login/auth/naver', passport.authenticate('naver', null),
-//         (req, res)=>{
-//             console.log('connect naver');
-//         })
-
-// app.get('/login/naver_oauth', passport.authenticate('naver', {
-//     failureRedirect : '/login'
-// }), (req, res, next)=>{
-//     console.log('logining');
-//     //res.end();
-//     //res.json({message : "success", username : req.user._name});
-//     res.redirect('/');
-//     //next();
-// })
-
-// app.get('/login/auth/kakao', passport.authenticate('kakao', null),
-//         (req, res)=>{
-//             console.log('connect kakao');
-//         })
-
-// app.get('/login/kakao_oauth', passport.authenticate('kakao', {
-//     failureRedirect : '/login'
-// }), (req, res, next)=>{
-//     console.log('logining');
-//     //res.end();
-//     res.redirect('/');
-//     //next();
-// })
-
-// app.get('/login/auth/facebook', passport.authenticate('facebook', null),
-//         (req, res)=>{
-//             console.log('connect facebook');
-//         })
-
-// app.get('/login/facebook_oauth', passport.authenticate('facebook', {
-//     failureRedirect : '/login'
-// }), (req, res, next)=>{
-//     console.log('logining');
-//     //res.end();
-//     res.redirect('/');
-//     //next();
-// })
-
+///////////route
 app.use('/', passport.passportRouter);
-
-
 
 app.get('/test', (req, res)=>{
     if(req.user != undefined){
@@ -279,6 +74,12 @@ app.get('/uesrs/:id', (req, res, next)=>{
     
     //next();
 })
+
+app.get('/login', (req, res, next)=>{
+    console.log('hello');
+
+    next();
+});
 
 app.all('/*', (req, res, next)=>{
    // res.sendFile(path.join(__dirname, 'view/index.html'));
@@ -330,9 +131,7 @@ app.get('/contact/result', (req, res)=>{
 
 
 
-app.get('/login', (req, res, next)=>{
-    console.log('hello');
-});
+
 
 app.get('/logout', (req, res)=>{
     req.logout();
