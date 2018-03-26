b2e0ddef930bed737e9a0f0b87e02761baa57d4a
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-26 15:56:35 +0900

    clean passport module

--- backend/module/users.js
+++ backend/module/users.js
@@ -1,22 +1,22 @@
 const Mongoose = require('mongoose');
 
-module.exports.User = () => {
-    const UserInfoSchema = new Mongoose.Schema({
-        _id : String,
-        _password : String,
-        _email : String,
-        _name : String
-    },{
-        versionKey : false
-    });
+const UserInfoSchema = new Mongoose.Schema({
+    _id : String,
+    _password : String,
+    _email : String,
+    _name : String
+},{
+    versionKey : false
+});
 
-    UserInfoSchema.methods.comparePassword = (inputPW, cb) => {
-        if (inputPW === this.password) {
-            cb(null, true);
-        } else {
-            cb('error');
-        }
-    };
+const UserModel = Mongoose.model('users', UserInfoSchema, 'users');
 
-    return Mongoose.model('users', UserInfoSchema, 'users');
-}
+UserInfoSchema.methods.comparePassword = (inputPW, cb) => {
+    if (inputPW === this.password) {
+        cb(null, true);
+    } else {
+        cb('error');
+    }
+};
+
+module.exports.User = UserModel;
