b4d47a938d97a897bf375d82c34b3cb4dc2bc707
Myeong Seok Ko <myeongsku@gmail.com>
2018-03-27 18:14:41 +0900

    josn test

--- backend/app.js
+++ backend/app.js
@@ -44,13 +44,13 @@ app.use('/', passport.passportRouter);
 
 app.get('/test', (req, res, next)=>{
     res.setHeader("Content-Type", "application/json");
-    
+
     if(req.user != undefined){
         User.findById(req.user._id, (err, user)=>{
             if(!err){
                 console.log("UserInfo : " + user._id);
-                res.render(path.join(__dirname, 'view/index.html'), user);
-                //res.json(user);
+                //res.render(path.join(__dirname, 'view/index.html'), user);
+                res.json(user);
                 //res.json({_name : "Test", _email : "", _id : "wefwefwefwef", _password : "wefwefwefwef"});
                 console.log('okokok');
             }
