16bf488a589ceabe456c64a714597e2918d6a4f4
Myeong Seok Ko <myeongsku@gmail.com>
2018-04-02 19:08:32 +0900

    edit user api

--- backend/app.js
+++ backend/app.js
@@ -46,26 +46,29 @@ app.get('/api/user/:id', (req, res)=>{//id 값의 유저 정보를 가져온다
     User.findOne({'_index' : req.param.id}, (err, user)=>{//id 값의 유저 정보를 찾는다
         
         if(!err){//정상 동작
+            if(req.user != undefined){//클라이언트가 로그인 상태 이며 현재 user와 동일할 때
 
-            delete user._id;
-            console.log("user!!!! : " + user);
+                delete user._id;
+                console.log("user!!!! : " + user);
 
-            if((req.user != undefined) && (req.user._index === req.param.id)){//클라이언트가 로그인 상태 이며 현재 user와 동일할 때
-                res.json({
-                    login_status : true,
-                    status : "",
-                    user_json : user
-                })
+                if(req.user._index === req.param.id){
+                    res.json({
+                        login_status : true,
+                        status : "",
+                        user_json : user
+                    })
+                }
+                else{//로그 아웃 상태 일 때
+                    res.json({
+                        login_status : false,
+                        status : "",
+                        user_json : user
+                    })
+                }
 
                 console.log('api/user/:id 출력!')
             }
-            else{//로그 아웃 상태 일 때
-                res.json({
-                    login_status : false,
-                    status : "",
-                    user_json : user
-                })
-            }
+            
         }
         else{//찾기 에러가 발생했을 때
             res.json({
